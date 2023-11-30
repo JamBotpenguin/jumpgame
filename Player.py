760cd398c4f0d0cc6a4e9e0b436ebb24c619b47b
JamBotpenguin <penguin+1801@penguincodingschool.com>
2023-11-30 17:29:25 -0500

    2nd class

--- Player.py
+++ Player.py
@@ -5,15 +5,31 @@ import pygame
 GRAVITY = 0.02
 class Player:
 
-    def __init__(self, color):
+    def __init__(self, color, w, h):
         self.c = color
         self.radius = 25
-        self.speed = 0
-        self.x = 400
+        self.yspeed = 0
+        self.xspeed = 0
+        self.x = 500
         self.y = 300
+        self.w = w
+        self.h = h
 
 
     def update(self, screen):
-        self.speed += GRAVITY
-        self.y += self.speed
+        self.yspeed += GRAVITY
+        self.y += self.yspeed
+        self.x += self.xspeed
+        self.wallcolision()
         pygame.draw.circle(screen, self.c,(self.x, self.y), self.radius )
+
+    def Jump(self):
+        self.yspeed = -3
+
+    def wallcolision(self):
+        if self.y <= 0 + self. radius:
+            self.y = 0 + self. radius
+            self.yspeed = 0.5
+        if self.y >= self.h - self. radius:
+            self.y = self.h - self. radius
+            self.yspeed = 0.5
--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from Player import Player
 pygame.init()
 
 BG_COLOR = (0, 255, 200)
-WIDTH = 800
+WIDTH = 1000
 HEIGHT = 600
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 screen.fill(BG_COLOR)
 pygame.display.flip()
 
-p1 = Player((200, 0, 0))
+p1 = Player((200, 0, 0),WIDTH, HEIGHT)
 
 running = True
 
@@ -20,6 +20,19 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                p1.Jump()
+            if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                p1.xspeed = -1
+            if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                    p1.xspeed = 1
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                p1.xspeed = -0
+            if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                p1.xspeed = 0
     p1.update(screen)
     pygame.time.wait(1)
     pygame.display.update()
+
